1b3d328e94403e3c740dbb678b22513746122407
francosoa <francosoamota@gmail.com>
2022-12-14 20:33:35 -0300

    tirando a duplicacao

--- operadores_logicos.js
+++ operadores_logicos.js
@@ -13,18 +13,15 @@ const listaDeViagem = new Array(
 //A condição abaixo exclui se um passageiro for menor de idade
 
 const comprador = 16
-const acompanhado = true
+const acompanhado = false
 
-//ifs encadeados
-if (comprador >= 18){
-    console.log("****Comprador maior de idade. Seu destino é: ****")
+//Tirando duplicação: 
+
+if (comprador >= 18 || acompanhado==true){
+    console.log("****Seu destino é: ****")
     console.log(listaDeViagem[1])
+    listaDeViagem.splice(1,1)
 }else{
-    if (acompanhado == true){
-        console.log("*****Passageiro(a) acompanhado: *****")
-        console.log(`Seu destino é: ${listaDeViagem[1]}*****`)
-    }else 
-        {console.log(`*******Venda não autorizada*******\n Motivo: Idade ${comprador}`)}
-    }
+    console.log(`*******Venda não autorizada*******\n Motivo: Idade ${comprador}`)
+}
 
-//ifs encadeados

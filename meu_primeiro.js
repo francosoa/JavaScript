212d713cbcd055ac13131dfc387652432fa8673b
francosoa <francosoamota@gmail.com>
2022-12-14 19:40:21 -0300

    meu primeiro progama

---
+++ meu_primeiro.js
@@ -0,0 +1,18 @@
+console.log("meu primeiro programa");
+
+
+const idade = 26;
+console.log(idade);
+console.log(idade + 10);
+
+
+console.log("*** Conversão de tipos ***");
+
+console.log("2" + "2");
+console.log(parseInt("2") + parseInt("2")); // parse muda o txt to int 
+
+console.log("Atribuição de variável");
+
+const nome = "Francisco"; //Não pode ser subescrita
+let sobrenome = "Mota"; //Pode ser subescrita
+console.log(`meu nome é ${nome} ${sobrenome}`); //precisa ser com crase

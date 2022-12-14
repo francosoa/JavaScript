b8bb181c0eed2bf800d1477a8bee7cb00f69a18b
francosoa <francosoamota@gmail.com>
2022-12-14 20:22:46 -0300

    if encadeados

--- operadores_logicos.js
+++ operadores_logicos.js
@@ -11,11 +11,20 @@ const listaDeViagem = new Array(
     );
 
 //A condição abaixo exclui se um passageiro for menor de idade
-const comprador = 16
 
+const comprador = 16
+const acompanhado = true
+
+//ifs encadeados
 if (comprador >= 18){
     console.log("****Comprador maior de idade. Seu destino é: ****")
     console.log(listaDeViagem[1])
 }else{
-    console.log(`*******Venda não autorizada*******\n Motivo: Idade ${comprador}`)
-}
+    if (acompanhado == true){
+        console.log("*****Passageiro(a) acompanhado: *****")
+        console.log(`Seu destino é: ${listaDeViagem[1]}*****`)
+    }else 
+        {console.log(`*******Venda não autorizada*******\n Motivo: Idade ${comprador}`)}
+    }
+
+//ifs encadeados

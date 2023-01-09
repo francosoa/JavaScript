62205cfc766d3e77027077439a217f7475e74579
francosoa <francosoamota@gmail.com>
2023-01-09 13:30:44 -0300

    media de array

---
+++ Alura/arrays/intro_arrays.js
@@ -0,0 +1,18 @@
+/*
+var --> tem um escopo global e de função. 
+Quando está num escopo global, significa que pode ser sua variável pode ser utilizada 
+em qualquer parte do código.
+No contexto de função, significa que a variável que foi declarada, só pode ser utilizada dentro 
+da função que foi construída
+
+Let --> é uma variável que funciona dentro de blocos.
+Bloco é o código declarado dentro de {}. O let ele pode ser atualizado, mas não declarado 
+novamente
+
+const --> é uma variável fixa, que não pode ser atualizada
+*/
+
+const notas = [10, 6.5, 8, 7.5];
+const media = (notas[0] + notas[1]+ notas[2] + notas[3]) / notas.length;
+
+console.log(media);

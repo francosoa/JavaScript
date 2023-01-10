d5ba8832e3b8854a1c532da1218f91cddf4c6303
francosoa <francosoamota@gmail.com>
2023-01-10 12:17:20 -0300

    utilizando o splice

--- Alura/arrays/alterando_array.js
+++ Alura/arrays/alterando_array.js
@@ -31,3 +31,12 @@ const sala2 = alunos.slice(alunos.length / 2); // quando não coloca o segundo p
 console.log(`alunos da sala1 ${sala1}`)
 console.log(`alunos da sala2 ${sala2}`)
 
+/* 
+Utilizando o splice:
+O Splice serve para remover itens de uma array com base no index.
+*/
+
+const nomes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
+
+nomes.splice(1, 2, "Rodrigo"); // O slice pede o index, qnts elementos vai remover a partir do index, e posso passar um terceiro paramentro
+console.log(nomes)
